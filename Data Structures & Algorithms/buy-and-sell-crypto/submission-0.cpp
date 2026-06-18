7a6bed79690a3fd3066f3fc688feb987512431e2
khimbodoi <171145075+khimbodoi@users.noreply.github.com>
2026-06-18 21:49:52 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                mx = max(prices[j] - prices[i], mx);
+            }
+        }
+        return mx;
+    }
+};
